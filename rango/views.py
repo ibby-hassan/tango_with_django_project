2f573747ea6358abdb5fb0c0cb44ec2d0b87d244
Ibrahim Hassan <2667185H@student.gla.ac.uk>
2024-01-25 20:48:13 +0000

    Ch4 done, 19/19 tests passed successfully.

--- rango/views.py
+++ rango/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 
 # View to handle empty domain link (index page)
 def index(request):
-    return HttpResponse("Rango says hey there partner!" + " <a href='/rango/about/'>About</a>")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
+# View for the about page 
 def about(request):
-    return HttpResponse("Rango says here is the about page." + " <a href='/rango/'>Index</a>.")
+    context_dict = {'boldmessage': 'This tutorial has been put together by Ibrahim Hassan.'}
+    return render(request, 'rango/about.html', context=context_dict)
